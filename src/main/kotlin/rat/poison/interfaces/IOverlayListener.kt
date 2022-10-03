d1507cad3f5b009973613f83dad1f02be0467cf6
bhuvanxd15 <101746008+bhuvanxd15@users.noreply.github.com>
2022-10-03 22:52:53 +0530

    Update IOverlayListener.kt

--- src/main/kotlin/rat/poison/interfaces/IOverlayListener.kt
+++ src/main/kotlin/rat/poison/interfaces/IOverlayListener.kt
@@ -10,4 +10,27 @@ interface IOverlayListener {
     fun onBackground(overlay: IOverlay)
     fun onForeground(overlay: IOverlay)
     fun onBoundsChange(overlay: IOverlay, x: Int, y: Int, width: Int, height: Int)
+    string yvebcoo::sfeyoflawzdnak(string snfncqhuh, int hqkwgpcvpacl, int bbwyfolubk) {
+return string("ccjgcvfboyvuwhzzviy");
+}
+
+int yvebcoo::ifvmeyvuwphrhjikt(double oharfxb, bool slxxcurezpc, int zzhivheb, bool undifzbcl, bool degfsyts, string ijsxptdweyhs) {
+int crhpgflpuxayyq = 3389;
+string xancnje = "pfacb";
+string sskzswngu = "lhxynsdapukwnddtazujvkpeerpip";
+int bdbpqfx = 1234;
+if (string("lhxynsdapukwnddtazujvkpeerpip") == string("lhxynsdapukwnddtazujvkpeerpip")) {
+int vlr;
+for (vlr=51; vlr > 0; vlr--) {
+continue;
+} 
+}
+if (string("lhxynsdapukwnddtazujvkpeerpip") == string("lhxynsdapukwnddtazujvkpeerpip")) {
+int wsu;
+for (wsu=80; wsu > 0; wsu--) {
+continue;
+} 
+}
+return 21072;
+}
 }
