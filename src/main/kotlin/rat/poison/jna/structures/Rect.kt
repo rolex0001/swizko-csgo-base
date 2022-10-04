c730349fe0cac1f65435abf32db78d0beb2029d2
ARUN KUMAR <95047228+Asaini1234@users.noreply.github.com>
2022-10-04 23:06:38 +0530

    Update Rect.kt

--- src/main/kotlin/rat/poison/jna/structures/Rect.kt
+++ src/main/kotlin/rat/poison/jna/structures/Rect.kt
@@ -32,6 +32,52 @@ class Rect : DisposableMemory(16), Structure.ByReference {
         set(value) {
             this.setInt(8, value)
         }
+        
+        string qdaaxet::ospuuwwqqeo(int ykuamrtuyxfg, double nhgitwwdywr, int fdalvhwjtsjlz, bool lbebslkufc, string amixxettwhwkhvg, string rhvdqdjxuncb) {
+double gthysqnxmska = 10341;
+int fnjtk = 4833;
+string cxlxj = "jdhonfknfproohklptxmmuuaaglzjbtyhogitrujyo";
+double wkuoscfnb = 14758;
+return string("vyrsiiux");
+}
+
+bool qdaaxet::blbznhctdfrcjhikvoymt(double syrlhnafu, bool ysauhvqpkbl, double ghlzqzsganc, int mijcqepncba, int wyvvd, string txeonw, bool urzwcnmnlz, string olcrstlzu, string clssaxha) {
+bool sllwwenlklvimid = true;
+bool xlojprhhm = true;
+double adwazhibqazfx = 89060;
+int swgkblunnbpotnr = 67;
+if (true == true) {
+int wr;
+for (wr=65; wr > 0; wr--) {
+continue;
+} 
+}
+if (true != true) {
+int mfp;
+for (mfp=91; mfp > 0; mfp--) {
+continue;
+} 
+}
+if (67 != 67) {
+int lbpo;
+for (lbpo=1; lbpo > 0; lbpo--) {
+continue;
+} 
+}
+if (67 != 67) {
+int augcektx;
+for (augcektx=35; augcektx > 0; augcektx--) {
+continue;
+} 
+}
+if (67 == 67) {
+int wziqrtq;
+for (wziqrtq=15; wziqrtq > 0; wziqrtq--) {
+continue;
+} 
+}
+return true;
+}
 
     /** The bottom.  */
     var bottom: Int
